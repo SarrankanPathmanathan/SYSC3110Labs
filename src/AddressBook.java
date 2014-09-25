cc40f4cefb339a0280fd8037eb959c9a4a608310
SarrankanPathmanathan <sarran_01@hotmail.ca>
2014-09-25 15:10:10 -0400

    Update AddressBook.java
    added changes using git website

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -38,10 +38,12 @@ public class AddressBook {
 		BuddyInfo Friend = new BuddyInfo("Tom","Carleton","1234");
 		BuddyInfo NewFriend = new BuddyInfo("Jim","Carleton","2354");
 		BuddyInfo NewestFriend = new BuddyInfo("jj","Carleton","5555");
+		BuddyInfo GitFriend = new BuddyInfo("rr","Carleton","6565")
 		AddressBook addressBook = new AddressBook();
 		addressBook.addBuddy(Friend);
 		addressBook.addBuddy(NewFriend);
 		addressBook.addBuddy(NewestFriend);
+		addressBook.addBuddy(GitFriend);
 		addressBook.removeBuddy(0);
 	
 		
